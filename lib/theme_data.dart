74c244683f95de3c2653fd9e3fb2e2f1bbeb4814
Samuel Matthew Koesnadi <samuelmat19@gmail.com>
2022-08-07 23:38:53 +0200

    Added Google Font

--- lib/theme_data.dart
+++ lib/theme_data.dart
@@ -1,5 +1,6 @@
 /// Set the theme of the application
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 const String fontName = 'Roboto';
@@ -8,5 +9,6 @@ final theme_data = ThemeData(
     primarySwatch: Colors.blue,
     brightness: Brightness.light,
     scaffoldBackgroundColor: const Color.fromRGBO(239, 238, 239, 1.0),
-    fontFamily: 'Roboto',
+    primaryTextTheme: GoogleFonts.getTextTheme('Lato'),
+    textTheme: GoogleFonts.getTextTheme('Lato'),
 );
